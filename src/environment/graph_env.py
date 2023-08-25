14705b56ee99c4384be34d3d0be9ac0deae75cc8
Andrea Bernini <andreabe99@gmail.com>
2023-08-25 17:49:10 +0200

    FIX: add a function to delete repeated edges.

--- src/environment/graph_env.py
+++ src/environment/graph_env.py
@@ -7,17 +7,21 @@ from src.community_algs.detection_algs import DetectionAlgorithm
 from src.utils import DetectionAlgorithms
 from src.utils import HyperParams
 
+from torch_geometric.utils import from_networkx
+from torch_geometric.data import Data
 from typing import List, Tuple
 import math
 import numpy as np
 import networkx as nx
+import torch
 
 
-class GraphEnviroment(object):
+
+class GraphEnvironment(object):
     """Enviroment where the agent will act, it will be a graph with a community"""
 
     def __init__(self, beta: float) -> None:
-        """Constructor for GraphEnviroment
+        """Constructor for Graph Environment
 
         Parameters
         ----------
@@ -53,7 +57,7 @@ class GraphEnviroment(object):
         # objective_function_values[0] --> Old Value, i.e. before the action
         # objective_function_values[1] --> New Value, i.e. after the action
         # The Value is the Deception Score
-        self.objective_function_values = np.zeros(2, dtype=np.float)
+        self.objective_function_values = np.zeros(2, dtype=float)
     
     @staticmethod
     def get_possible_actions(
@@ -80,14 +84,23 @@ class GraphEnviroment(object):
         def out_community(node):
             return node not in community
 
-        for u, v, _ in graph.edges(data=True):
-            # We can remove an edge iff both nodes are in the community
-            if in_community(u) and in_community(v):
-                possible_actions["REMOVE"].append((u,v))
-            # We can add an edge iff one node is in the community and the other is not
-            elif (in_community(u) and out_community(v)) \
-                or (out_community(u) and in_community(v)):
-                possible_actions["ADD"].append((u,v))
+        for u in graph.nodes():
+            for v in graph.nodes():
+                if u == v:
+                    continue
+                # We can remove an edge iff both nodes are in the community
+                if in_community(u) and in_community(v):
+                    if graph.has_edge(u,v):
+                        if (v, u) not in possible_actions["REMOVE"]:
+                            possible_actions["REMOVE"].append((u,v))
+                            
+                # We can add an edge iff one node is in the community and the other is not
+                elif (in_community(u) and out_community(v)) \
+                    or (out_community(u) and in_community(v)):
+                    # Check if there is already an edge between the two nodes
+                    if not graph.has_edge(u,v):
+                        if (v, u) not in possible_actions["ADD"]:
+                            possible_actions["ADD"].append((u,v))
         
         return possible_actions
     
@@ -108,7 +121,7 @@ class GraphEnviroment(object):
         int
             Edge budgets of the graph
         """
-        return int(math.ceil((graph.n_edges * beta / 100)))
+        return int(math.ceil((graph.number_of_edges() * beta / 100)))
 
     @staticmethod
     def get_reward( 
@@ -132,15 +145,59 @@ class GraphEnviroment(object):
         """
         reward = weight * deception_score + (1 - weight) * nmi_score
         return reward
+
+    def delete_repeat_edges(self, data: Data) -> torch.Tensor:
+        """The Data object contains the edge_index tensor, which is a tensor
+        of size 2*N, where N is the number of edges. The first row of the tensor
+        contains the indices of the source nodes, and the second row contains
+        the indices of the destination nodes. Each row of the tensor represents
+        an edge in the graph. This function removes the duplicate edges, i.e.
+        the edges that are present in both directions, e.g. (0, 1) and (1, 0)
+
+        Parameters
+        ----------
+        data : Data
+            Graph before the duplicate edges have been removed
+
+        Returns
+        -------
+        data : Data
+            Graph after the duplicate edges have been removed
+        """
+        # TODO: Remove duplicate edges, without reshaping the tensor
+        # Reshape the edge_index tensor in N*2
+        edge_index = data.edge_index.t().contiguous()
+        # Remove the duplicate edges
+        sorted_edge_index, _ = torch.sort(edge_index, dim=1)
+        edge_index = torch.unique(sorted_edge_index, dim=0)
+        data.edge_index = edge_index.t().contiguous()
+        return data
     
-    def apply_action(self, action:Tuple[int, int])->int:
+    def reset(self) -> Data:
+        """Reset the environment
+
+        Returns
+        -------
+        Data
+            Graph after the reset
+        """
+        self.used_edge_budget = 0
+        self.exhausted_budget = False
+        self.graph = self.graph_copy.copy()
+        # self.rewards = 0.0
+        
+        data = self.delete_repeat_edges(from_networkx(self.graph))
+        return data
+    
+    def apply_action(self, actions: List[float])->int:
         """Applies the action to the graph, if there is an edge between the two 
         nodes, it removes it, otherwise it adds it
 
         Parameters
         ----------
-        graph : nx.Graph
-            NetworkX Graph object to apply the action to
+        actions : List[float]
+            List of possible actions, where each element is a real number
+            between 0 and 1
         
         Returns
         -------
@@ -150,12 +207,41 @@ class GraphEnviroment(object):
         # Check if between the two nodes there is an edge:
         #   - If there is an edge, it means that the action is to remove it
         #   - If there is no edge, it means that the action is to add it 
-        if action in self.possible_actions["REMOVE"]:
-            self.graph.remove_edge(action[0], action[1])
+
+        # Get the index of the maximum value in the action list
+        index = actions.index(max(actions))
+        # Get the action to apply from self.possible_actions
+        # NOTE: the number of possible actions is: 
+        #   len(self.possible_actions["ADD"]) + len(self.possible_actions["REMOVE"])
+        # So, if the index is less than the number of possible actions to add,
+        # the action to apply is an action to add, otherwise it is an action to remove
+        if index < len(self.possible_actions["ADD"]):
+            action = self.possible_actions["ADD"][index]
+        else:
+            action = self.possible_actions["REMOVE"][index - len(self.possible_actions["ADD"])]
+        
+        # TODO: Join th following code with the code above
+        # Check if the action is to add or to remove an edge
+        if action == (-1,-1):
+            budget_consumed = 0
+        elif action in self.possible_actions["REMOVE"]:
+            self.graph.remove_edge(*action)
+            # Replace the removed edge with (-1,-1) in the possible actions, 
+            # in order to keep the same length, and to avoid to remove the same
+            # edge multiple times
+            idx = self.possible_actions["REMOVE"].index(action)
+            self.possible_actions["REMOVE"][idx] = (-1,-1)
             budget_consumed = 1
+            # print("Remove", action)
         elif action in self.possible_actions["ADD"]:
-            self.graph.add_edge(action[0], action[1])
+            self.graph.add_edge(*action, weight=1)
+            # Replace the added edge with (-1,-1) in the possible actions, in this way
+            # we can keep track of the used actions, and we can avoid to add the same
+            # edge multiple times
+            idx = self.possible_actions["ADD"].index(action)
+            self.possible_actions["ADD"][idx] = (-1,-1)
             budget_consumed = 1
+            # print("Add", action)
         else:
             budget_consumed = 0
         return budget_consumed
@@ -180,6 +266,7 @@ class GraphEnviroment(object):
             Whether the environment is used for training, by default False
         """
         self.graph = graph
+        self.graph_copy = graph.copy()
         self.community_target = community
         self.training = training
         self.rewards = 0.0
@@ -197,7 +284,7 @@ class GraphEnviroment(object):
         
         # Compute the edge budget for the graph
         self.edge_budget = self.get_edge_budget(self.graph, self.beta)
-        self.used_edge_budget = 0.0
+        self.used_edge_budget = 0
         self.exhausted_budget = False
         
         # Compute the set of possible actions
@@ -210,31 +297,31 @@ class GraphEnviroment(object):
         # if self.training:
         #    self.objective_function_values[0] = self.objective_function_values[0] * self.rewards_scale_multiplier 
     
-    def step(self, action: Tuple(int, int)) -> Tuple[float, bool]:
+    def step(self, actions: List[float]) -> Tuple[Data, float]:
         """Step function for the environment
 
         Parameters
         ----------
-        action : Tuple(int, int)
-            Action to take on the graph, which is a tuple containing the
-            nodes to rewire
-
+        actions : List[float]
+            Actions to take on the graph, which is a list longer as the number
+            of possible actions, where each element is a real number between
+            0 and 1
         Returns
         -------
-        Tuple[float, bool]
-            Tuple containing the reward and whether the episode is done
+        self.graph, self.rewards: Tuple[Data, float]
+            Tuple containing the new graph and the reward 
         """
         
-        # Check if the budget of rewiring has been exhausted
-        if self.exhausted_budget:
-            print("Budget exhausted")
-            return self.rewards, True
-        
+                
         # Compute the remaining budget
         remaining_budget = self.edge_budget - self.used_edge_budget
         
+        if remaining_budget <= 0:
+            self.exhausted_budget = True
+            return self.graph, self.rewards
+        
         # Take action
-        budget_consumed = self.apply_action(action)
+        budget_consumed = self.apply_action(actions)
         # Decrease the remaining budget
         updated_budget = remaining_budget - budget_consumed
         
@@ -261,10 +348,13 @@ class GraphEnviroment(object):
         # if self.training:
         #    self.objective_function_values[1] *= self.rewards_scale_multiplier
         
-        # Compute the reward as the difference between the Decption Score 
+        # Compute the reward as the difference between the Deception Score
         # before and after the action
         # reward = self.objective_function_values[0] - self.objective_function_values[1]
         
         if abs(reward) < self.eps:
             reward = 0.0
         self.rewards = reward
+        
+        data = self.delete_repeat_edges(from_networkx(self.graph))
+        return data, self.rewards

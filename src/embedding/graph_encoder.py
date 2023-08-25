39b52699098a80f411a46725fcc67510e78d7abd
Andrea Bernini <andreabe99@gmail.com>
2023-08-25 17:47:39 +0200

    FIX: modify the forward function.
    We modify the `forward` function to handle the argument as a tensor.

--- src/embedding/graph_encoder.py
+++ src/embedding/graph_encoder.py
@@ -17,14 +17,90 @@ class GraphEncoder(nn.Module):
         self.linear1 = nn.Linear(hidden_feature,out_feature)
         self.tanh = nn.Tanh()
         self.relu = torch.relu
-
-    def forward(self,  Graph):
-
-        x, edge_index,batch = Graph.x, Graph.edge_index,Graph.batch
+    
+    def forward(self, edge_index: torch.Tensor)-> torch.Tensor:
+        n_nodes = edge_index.max().item() + 1
+        x = torch.randn([n_nodes, 50])
+        batch = torch.zeros(n_nodes).long()
+        
         x = self.conv1(x, edge_index)
         x = self.relu(x)
-        embedding = global_mean_pool(x,batch)
+        embedding = global_mean_pool(x, batch)
         embedding = self.linear1(embedding)
         embedding = self.tanh(embedding)
 
         return embedding
+    
+    """
+    def forward(self, graph: Data)-> torch.Tensor:
+        # Features Matrix
+        x = graph.x
+        
+        # The graph is defined using an adjacency matrix. 
+        # The first row of the tensor contains the indices of the source nodes, 
+        # and the second row contains the indices of the destination nodes. 
+        # Each row of the tensor represents an edge in the graph.
+        edge_index = graph.edge_index
+        
+        # The batch tensor is used to indicate which nodes belong to which
+        # graphs in a batch. In this case, there is only one graph, so the
+        # batch tensor is a tensor of zeros with shape (num_nodes, 1).
+        batch = graph.batch
+        
+        if x is None:
+            x = torch.randn([graph.num_nodes, 50])
+        # Check if the batch tensor is None
+        if batch is None:
+            # batch = torch.zeros([graph.num_nodes, 1]).long()
+            batch = torch.zeros(graph.num_nodes).long()
+        
+        x = self.conv1(x, edge_index)
+        x = self.relu(x)
+        embedding = global_mean_pool(x, batch)
+        embedding = self.linear1(embedding)
+        embedding = self.tanh(embedding)
+
+        return embedding
+    """
+    
+    """
+    def forward(self, edge_index: torch.Tensor):
+        # Features Matrix
+        # x = graph.x
+        
+        # The graph is defined using an adjacency matrix. 
+        # The first row of the tensor contains the indices of the source nodes, 
+        # and the second row contains the indices of the destination nodes. 
+        # Each row of the tensor represents an edge in the graph.
+        # edge_index = graph.edge_index
+        
+        # TODO: Remove duplicate edges, without reshaping the tensor
+        # Reshape the edge_index tensor in N*2
+        edge_index = edge_index.t().contiguous()
+        # Remove the duplicate edges
+        sorted_edge_index, _ = torch.sort(edge_index, dim=1)
+        edge_index = torch.unique(sorted_edge_index, dim=0)
+        edge_index = edge_index.t().contiguous()
+        
+        # The batch tensor is used to indicate which nodes belong to which 
+        # graphs in a batch. In this case, there is only one graph, so the 
+        # batch tensor is a tensor of zeros with shape (num_nodes, 1).
+        # batch = graph.batch
+        
+        n_nodes = edge_index.max().item() + 1
+        # Check if the features matrix is None
+        # if x is None:
+        x = torch.randn([n_nodes, 50])
+        # Check if the batch tensor is None
+        # if batch is None:
+        # batch = torch.zeros([graph.num_nodes, 1]).long()
+        batch = torch.zeros(n_nodes).long()
+        
+        x = self.conv1(x, edge_index)
+        x = self.relu(x)
+        embedding = global_mean_pool(x, batch)
+        embedding = self.linear1(embedding)
+        embedding = self.tanh(embedding)
+
+        return embedding
+    """

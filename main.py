f85e3bd9df2e8dfc3aa8da701ebaa20baaac1439
AndreaBe99 <andreabe99@gmai.com>
2023-09-21 21:25:44 +0200

    FEAT: handle Node and Community hiding task

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from src.utils.utils import HyperParams, Utils, FilePaths, DetectionAlgorithmsNa
 from src.environment.graph_env import GraphEnvironment
 from src.agent.agent import Agent
 
-from src.utils.test import test
-from src.utils.community_deception import community_deception
+from src.utils.hiding_node import NodeHiding
+from src.utils.hiding_community import CommunityHiding
+
 import argparse
 import math
 
@@ -18,25 +19,27 @@ def get_args():
     """
     parser = argparse.ArgumentParser(description='PyTorch A2C')
     # Mode: train or test
-    parser.add_argument('--mode', type=str, default='train',
-                        help='train or test')
+    parser.add_argument('--mode', type=str, default='both',
+                        help='train | test | both')
     # Argument parsing
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = get_args()
-    # NOTE To modify the hyperparameters, dataset, detection algorithm, etc. 
-    # NOTE  please refer to the file src/utils/utils.py in the class HyperParams
+    
     # ° --- Environment Setup --- ° #
     env = GraphEnvironment()
-    # ° ------ Agent Setup ----- ° #
+    
+    # ° ------  Agent Setup ----- ° #
     agent = Agent(env=env)
-    # ° ------ TRAIN ------ ° #
-    if args.mode == "train":
+    
+    # ° ------    TRAIN    ------ ° #
+    if args.mode == "train" or args.mode == "both":
         # Training
         agent.grid_search()
-    # ° ------ TEST ------ ° #
-    elif args.mode == 'test':
+    
+    # ° ------    TEST    ------ ° #
+    elif args.mode == 'test' or args.mode == "both":
         # To change the detection algorithm, or the dataset, on which the model
         # will be tested, please refer to the class HyperParams in the file
         # src/utils/utils.py, changing the values of the variables:
@@ -47,18 +50,33 @@ if __name__ == "__main__":
         # file src/utils/utils.py
         model_path = FilePaths.TRAINED_MODEL.value
         
-        # Change the beta and tau parameters on which the model will be tested
-        betas = [HyperParams.BETA.value]  # [1,3,5]
+        # Tau defines the strength of the constraint on the goal achievement
         taus = [0.3, 0.5, 0.8]
+        # BETAs defines the number of actions to perform
+        # Beta for the community hiding task defines the percentage of rewiring 
+        # action, add or remove edges
+        community_betas = [10, 20, 30]
+        # Beta for the node hiding task is a multiplier of mean degree of the
+        # the graph
+        node_betas = [1, 2, 3]  # [1,3,5]
         
-        # Get communty target
-        community_target = agent.env.community_target
-        for beta in betas:
-            for tau in taus:
-                print("* * Testing with beta = {} and tau = {}".format(beta, tau))
-                test(agent=agent, model_path=model_path, beta=beta, tau=tau)
-                # print("* * Community Hiding with beta = {} and tau = {}".format(beta, tau))
-                # community_deception(agent=agent, community_target=community_target, beta=beta, tau=tau, model_path=model_path)
-                print("*"*50)
+        # Initialize the test class
+        node_hiding = NodeHiding(agent=agent, model_path=model_path)
+        community_hiding = CommunityHiding(agent=agent, model_path=model_path)
+        
+        for tau in taus:
+            
+            print("* Node Hiding with tau = {}".format(tau))
+            for beta in node_betas:
+                print("* * Beta = {}".format(beta))
+                node_hiding.set_parameters(beta=beta, tau=tau)
+                node_hiding.run_experiment()
+                
+            print("* Community Hiding with tau = {}".format(tau))
+            for beta in community_betas:
+                print("* * Beta = {}".format(beta))
+                community_hiding.set_parameters(beta=beta, tau=tau)
+                community_hiding.run_experiment()
+            print("* "*50)
     else:
         raise ValueError("Invalid mode. Please choose between 'train' and 'test'")
